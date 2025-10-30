80def69cbcf3b1e1d8335a7ab6640ad1af85774e
congsoony <44600941+congsoony@users.noreply.github.com>
2025-10-30 17:08:25 +0900

    Title: 최대 거리의 점, Time: 76ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 251030/최대 거리의 점/maximum-distance-point.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[200000];
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int res = 0;
+    int l =1,r=1e9;
+    while(l<=r){
+        int mid = (l+r)/2;
+        int cnt =1;
+        int pre=arr[0];
+        for(int i=1;i<n;i++){
+            if(arr[i]-pre>=mid){
+                cnt++;
+                pre=arr[i];
+            }
+        }
+        if(cnt>=m){
+            l=mid+1;
+            res = max(res,mid);
+        }
+        else{
+            r=mid-1;
+        }
+    }
+    cout<<res<<"\n";
+
+    return 0;
+}
