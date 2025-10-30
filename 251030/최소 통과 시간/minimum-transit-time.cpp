a10282930d0fe9f2dcb659a4e0f0c593c02f750f
congsoony <44600941+congsoony@users.noreply.github.com>
2025-10-30 17:29:14 +0900

    Title: 최소 통과 시간, Time: 21ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251030/최소 통과 시간/minimum-transit-time.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+int arr[100000];
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> arr[i];
+    }
+    int l = 1,r=1e9;
+    int res = 1e9;
+    while(l<=r){
+        int mid = (l+r)/2;
+        int cnt = 0;
+        for(int i =0;i<m;i++){
+            cnt+=mid/arr[i];
+        }
+        if(cnt>=n){
+            r = mid-1;
+            res = min(res,mid);
+        }
+        else{
+            l=mid+1;
+        }
+    }
+    cout<<res<<"\n";
+    return 0;
+}
