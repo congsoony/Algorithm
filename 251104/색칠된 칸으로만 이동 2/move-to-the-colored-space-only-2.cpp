d4051ecc01ca8531b5a62fe26cf7b1df8c6a72b8
congsoony <44600941+congsoony@users.noreply.github.com>
2025-11-04 10:52:17 +0900

    Title: 색칠된 칸으로만 이동 2, Time: 23ms, Memory: 0MB, Status: Passed - Codetree

--- 251104/색칠된 칸으로만 이동 2/move-to-the-colored-space-only-2.cpp
+++ 251104/색칠된 칸으로만 이동 2/move-to-the-colored-space-only-2.cpp
@@ -5,19 +5,20 @@ int color[100][100];
 int n,m;
 int dy[4]={-1,0,1,0};
 int dx[4]={0,1,0,-1};
+vector<pair<int,int>> v;
 bool isrange(int y,int x){
     return 0<=y&&y<n&&0<=x&&x<m;
 }
-int bfs(int y,int x,int y2,int x2,int dis){
+int bfs(int dis){
 
     vector<vector<int>>dist(n,vector<int>(m,1e9));
     queue<pair<int,int>> q;
-    q.push({y,x});
-    dist[y][x]=0;
+    q.push({v[0].first,v[0].second});
+    dist[v[0].first][v[0].second]=0;
     while(!q.empty()){
+        int y,x;
         tie(y,x)=q.front();
         q.pop();
-        if(y==y2&&x==x2)return 1;
         for(int i =0;i<4;i++){
             int ny=y+dy[i];
             int nx=x+dx[i];
@@ -30,7 +31,11 @@ int bfs(int y,int x,int y2,int x2,int dis){
             }
         }
     }
-    return 0;
+
+    for(auto[y,x]:v){
+        if(dist[y][x]==1e9)return 0;
+    }
+    return 1;
 }
 int main() {
     freopen("input.txt","r",stdin);
@@ -40,7 +45,7 @@ int main() {
             cin>>board[i][j];
         }
     }
-    vector<pair<int,int>> v;
+
     for(int i =0;i<n;i++){
         for(int j =0;j<m;j++){
             cin>>color[i][j];
@@ -48,21 +53,12 @@ int main() {
                 v.push_back({i,j});
         }
     }
+    
     int l=1,r=1e9;
     int res = 1e9;
     while(l<=r){
         int mid = (l+r)/2;
-        int size = v.size();
-        int flag = 1;
-        for(int i =0;i<size-1;i++){
-            for(int j =i+1;j<size;j++){
-                flag&=bfs(v[i].first,v[i].second,v[j].first,v[j].second,mid);
-
-                if(flag==0)break;
-            }
-            if(flag==0)break;
-        }
-        if(flag){
+        if(bfs(mid)){
             r = mid-1;
             res = min(res,mid);
         }
