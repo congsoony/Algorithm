fe10f1f25e1d4367a4d9eb38a11cbb9f14b9ddf7
congsoony <44600941+congsoony@users.noreply.github.com>
2025-12-30 12:40:57 +0900

    Title: 닷 투더 닷, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251230/닷 투더 닷/dot-to-the-dot.cpp
+++ 251230/닷 투더 닷/dot-to-the-dot.cpp
@@ -3,36 +3,43 @@ using namespace std;
 #define ll long long
 ll n,m,x;
 vector<tuple<ll,ll,ll>> v[1001];
+ll dijkstra(ll  c){
+    priority_queue<tuple<ll,ll>> pq;
+    pq.push({0,1});
+    vector<ll>dist(n+1,1e15);
+    dist[1]=0;
+    while(!pq.empty()){
+        ll cost ,cur;
+        tie(cost,cur)= pq.top();
+        pq.pop();
+        cost = -cost;
+        if(dist[cur]<cost)continue;
+        for(auto[next,L,C]:v[cur]){
+            if(C<c)continue;
+            ll ncost = cost+L;
+            if(dist[next]>ncost){
+                pq.push({-ncost ,next});
+                dist[next]=ncost;
+            }
+        }
+    }
+    return dist[n];
+}
 int main() {
     freopen("input.txt","r",stdin);
     cin>>n>>m>>x;
     ll a,b,c,d;
+    set<ll> cs;
     for(int i =0;i<m;i++){
         cin>>a>>b>>c>>d;
         v[a].push_back({b,c,d});
         v[b].push_back({a,c,d});
+        cs.insert(d);
     }
-    queue<tuple<ll,ll,ll>> q;
-    q.push({1,0,1e15});
-    vector<ll>dist(n+1,1e15);
-    dist[1]=0;
-
-    while(!q.empty()){
-        ll cur,sum,minc;
-        tie(cur,sum,minc)= q.front();
-        q.pop();
-        ll cost = sum+minc/x;
-
-
-        for(auto[next,L,C]:v[cur]){
-            ll nextsum  = sum + L;
-            ll nextC = min(minc,C);
-            ll nextcost = nextsum + x / nextC;
-            if(dist[next]>=nextcost){
-                q.push({next,nextsum,nextC});
-                dist[next]=nextcost;
-            }
-        }
+    
+    ll res =1e15;
+    for(ll t : cs){
+        res = min(dijkstra(t) + x/t,res);
     }
-    cout<<dist[n]<<"\n";
+    cout<<res<<"\n";
 }
