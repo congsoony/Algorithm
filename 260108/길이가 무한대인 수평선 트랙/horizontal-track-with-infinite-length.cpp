cc7c962e1eaa275560a9593087acefd9ba83d0a6
congsoony <44600941+congsoony@users.noreply.github.com>
2026-01-08 15:20:27 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 29ms, Memory: 1MB, Status: Passed - Codetree

--- 260108/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.cpp
+++ 260108/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.cpp
@@ -8,14 +8,18 @@ int main() {
     freopen("input.txt","r",stdin);
     cin>>n>>t;
     ll a,b;
-    set<ll> s;
+    vector<ll> v(n);
     for(int i =0;i<n;i++){
         cin>>a>>b;
-        ll c = a+b*t;
-        auto it = s.lower_bound(c);
-        if(it!=s.end())
-            s.erase(it);
-        s.insert(c);
+        v[i]=a + b*t;
     }
-    cout<<s.size()<<"\n";
+    ll cur = 2e18;
+    int res = 0;
+    for(int i =n-1;i>=0;i--){
+        if(v[i]<cur){
+            res++;
+            cur= v[i];
+        }
+    }
+    cout<<res<<"\n";
 }
