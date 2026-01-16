6d58de876c2b56bb9c654591f47a6ac65f4027d7
congsoony <44600941+congsoony@users.noreply.github.com>
2026-01-16 15:51:43 +0900

    Title: 폭탄 해체 작업, Time: 323ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 260116/폭탄 해체 작업/the-bomb-dismantling.java
+++ 260116/폭탄 해체 작업/the-bomb-dismantling.java
@@ -6,6 +6,12 @@ public class Main {
         System.setIn(new FileInputStream("input.txt"));
         Scanner sc = new Scanner(System.in);
         List<int[]>l =new ArrayList<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+            if(a[0]==b[0]){
+                return a[1]-b[1];
+            }
+            return b[0]-a[0];
+        });
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
             int a=sc.nextInt();
@@ -18,13 +24,34 @@ public class Main {
             }
             return a[1]-b[1];
         });
+
+        int arr[]=new int[10001];
         int res =l.get(0)[0];
         int before=l.get(0)[1];
+        arr[before] = res;
         for(int i =1;i<n;i++){
             if(before <l.get(i)[1]){
-                res+=l.get(i)[0];
                 before = l.get(i)[1];
+                arr[before]=l.get(i)[0];
             }
+            else{
+                pq.add(l.get(i));
+            }
+        }
+        for(int i =1;i<=10000;i++){
+            if(pq.isEmpty())break;
+            int p[] = pq.peek();
+            if(p[1]<i) {
+                pq.poll();
+            }
+            else if(p[0]>arr[i]){
+                arr[i]=p[0];
+                pq.poll();
+            }
+        }
+        res =0;
+        for(int i =0;i<=10000;i++){
+            res+=arr[i];
         }
         System.out.println(res);
     }
