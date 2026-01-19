1a53ff285f408d4639e296f9f5ad0444e177b6b2
congsoony <44600941+congsoony@users.noreply.github.com>
2026-01-19 12:54:18 +0900

    Title: 자연수 M/2개의 쌍, Time: 1618ms, Memory: 56MB, Status: Passed - Codetree

--- 260119/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
+++ 260119/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
@@ -7,20 +7,34 @@ public class Main {
         System.setIn(new FileInputStream("input.txt"));
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        List<Integer> l = new ArrayList<>();
+        List<int[]> l = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
-            while(x-- >0){
-                l.add(y);
-            }
+            l.add(new int[]{x,y});
         }
-        l.sort((a,b)->a-b);
+        l.sort((a,b)->a[1]-b[1]);
 
         int left=0,right = l.size()-1;
         int res = 0;
-        while(left<right){
-            res = Math.max(res,l.get(left++)+l.get(right--));
+        while(left<=right){
+            int cnt1 = l.get(left)[0];
+            int val1 = l.get(left)[1];
+            int cnt2 = l.get(right)[0];
+            int val2 = l.get(right)[1];
+            res= Math.max(res,val1+val2);
+            if(cnt1<cnt2){
+                left++;
+                l.set(right,new int[]{cnt2-cnt1,val2});
+            }
+            else if(cnt1>cnt2){
+                right--;
+                l.set(left,new int[]{cnt1-cnt2,val1});
+            }
+            else{
+                left++;
+                right--;
+            }
         }
         System.out.println(res);
     }
