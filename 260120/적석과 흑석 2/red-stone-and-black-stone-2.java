f223771371d75345c28fa1ff515fadc1f694c514
congsoony <44600941+congsoony@users.noreply.github.com>
2026-01-20 21:31:51 +0900

    Title: 적석과 흑석 2, Time: 1190ms, Memory: 42MB, Status: Passed - Codetree

--- 260120/적석과 흑석 2/red-stone-and-black-stone-2.java
+++ 260120/적석과 흑석 2/red-stone-and-black-stone-2.java
@@ -13,33 +13,26 @@ public class Main {
             redStones[i] = sc.nextInt();
         }
         int[][] blackStones = new int[n][2];
-        TreeSet<int[]>tr =new TreeSet<>((a,b)->{
-            if(a[0]==b[0])return a[1]-b[1];
-            return a[0]-b[0];
-        });
-
         for (int i = 0; i < n; i++) {
             blackStones[i][0] = sc.nextInt();
             blackStones[i][1] = sc.nextInt();
-            tr.add(new int[]{-blackStones[i][0],blackStones[i][1]});
-
         }
+        Arrays.sort(blackStones, (a,b)->a[0]-b[0]);
         Arrays.sort(redStones);
         int res = 0;
-
-//        for(int i=0;i<c;i++){
-//            System.out.println(redStones[i]);
-//        }
-        //for(int t[] :tr) System.out.println(-t[0]+" "+t[1]);
-
+        PriorityQueue<Integer>pq= new PriorityQueue<>();
+        int idx = 0;
         for(int i =0;i<c;i++){
-            int t[]=tr.ceiling(new int[]{-redStones[i],redStones[i]});
-            if(t!=null){
-                if(redStones[i]<=t[1]){
-                    tr.remove(t);
-                    res++;
-
-                }
+            while(idx<n &&blackStones[idx][0]<=redStones[i]){
+                pq.add(blackStones[idx][1]);
+                idx++;
+            }
+            while(!pq.isEmpty()&& pq.peek()<redStones[i]){
+                pq.poll();
+            }
+            if(!pq.isEmpty()){
+                res++;
+                pq.poll();
             }
         }
         System.out.println(res);
