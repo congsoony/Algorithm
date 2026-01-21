7f7c7c3239c15e110deb1cc1aa6c2b226c9768ea
congsoony <44600941+congsoony@users.noreply.github.com>
2026-01-21 10:29:36 +0900

    Title: G & H 반전시키기 3, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 260121/G & H 반전시키기 3/reversing-g-and-h-3.java
+++ 260121/G & H 반전시키기 3/reversing-g-and-h-3.java
@@ -1,6 +1,9 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("input.txt"));
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String a = sc.next();
@@ -10,16 +13,17 @@ public class Main {
         for(int i =0;i<n;i++){
             arr1[i]=a.charAt(i)=='G'?1:0;
             arr2[i]=b.charAt(i)=='G'?1:0;
-            
+
         }
         int res = 0;
         for(int i =n-1;i>=0;i--){
             if(arr1[i]!=arr2[i]){
                 res++;
-                for(int j=0;j<4 &&i-j>=0;j++){
-                    if(arr1[i-j]==arr2[i-j])break;
-                    i--;
+                for(int j=0;j<4 &&i>=0;j++,i--){
+                    if(arr1[i]==arr2[i])break;
+                    arr1[i]=arr2[i];
                 }
+                i++;
             }
         }
         System.out.println(res);
