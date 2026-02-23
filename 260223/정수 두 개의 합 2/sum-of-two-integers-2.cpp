b40e7b5d8ff57479e554f541864d16e8eeab2838
congsoony <44600941+congsoony@users.noreply.github.com>
2026-02-23 16:50:14 +0900

    Title: 정수 두 개의 합 2, Time: 35ms, Memory: 0MB, Status: Passed - Codetree

--- 260223/정수 두 개의 합 2/sum-of-two-integers-2.cpp
+++ 260223/정수 두 개의 합 2/sum-of-two-integers-2.cpp
@@ -1,14 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
+    freopen("input.txt","r",stdin);
     int n,k;
     cin>>n>>k;
     vector<int>v(n);
     for(int &a:v)cin>>a;
     sort(v.begin(),v.end());
     long long res = 0;
-    for(int i =0;i<n;i++){
-        res+=upper_bound(v.begin()+i,v.end(),k-v[i])-lower_bound(v.begin()+i,v.end(),k-v[i]);
+    int l=0,r=n-1;
+    while(l<r){
+        if(v[l]+v[r]>k){
+            r--;
+        }
+        else{
+            res+=r-l;
+            l++;
+        }
     }
     cout<<res<<"\n";
 }
