2062f321b918851ef36efa12002020f52a783677
congsoony <44600941+congsoony@users.noreply.github.com>
2026-02-24 12:00:22 +0900

    Title: 0에 가장 가까운 합, Time: 21ms, Memory: 0MB, Status: Passed - Codetree

--- 260224/0에 가장 가까운 합/sum-closest-to-zero.cpp
+++ 260224/0에 가장 가까운 합/sum-closest-to-zero.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int n;
 int main() {
-    //freopen("input.txt","r",stdin);
+    freopen("input.txt","r",stdin);
     cin>>n;
     vector<int>v(n);
     for(int &a:v)cin>>a;
@@ -11,11 +11,11 @@ int main() {
 
     int res =2e9;
     while(l<r){
-        if(v[r]+v[l]>0){
+        if(v[r]+v[l]>=0){
             res = min(res,abs(v[r--]+v[l]));
         }
         else{
-            res = min(res,abs(v[r]+v[l--]));
+            res = min(res,abs(v[r]+v[l++]));
         }
     }
     cout<<res<<"\n";
