89b8d24f2e4e3c5d5832474bb7a8fd3cde9e18b9
congsoony <44600941+congsoony@users.noreply.github.com>
2026-02-24 15:16:56 +0900

    Title: 특정 조건으로 묶여있는 수 그룹, Time: 29ms, Memory: 1MB, Status: Passed - Codetree

--- 260224/특정 조건으로 묶여있는 수 그룹/a-group-of-numbers-tied-to-specific-conditions.cpp
+++ 260224/특정 조건으로 묶여있는 수 그룹/a-group-of-numbers-tied-to-specific-conditions.cpp
@@ -5,27 +5,28 @@ int main() {
     freopen("input.txt","r",stdin);
     int n,k;
     cin>>n>>k;
-    vector<int> v(n);
+    vector<int> v(n),pre(n),suf(n);
     for(int i =0;i<n;i++)cin>>v[i];
     sort(v.begin(),v.end());
     int l = 0;
-    int res = 0;
-    int mx = v[0];
-    int mn = v[0];
-    vector<int> ans;
-    for(int i =1;i<n;i++){
-        if(abs(mx-v[i])>k || abs(mn-v[i])>k){
-            if(i-1 >l){
-                ans.push_back(i-l);
-            }
-            mx = mn = v[i];
-            l=i;
-        }
-        else{
-            mx= max(mx,v[i]);
-            mn = min(mn,v[i]);
-        }
+
+    for(int i = 0;i<n;i++){
+        while(v[i]-v[l]>k)l++;
+        int len = i-l+1;
+        pre[i] = max((i ? pre[i - 1] : 0), len);
     }
-    sort(ans.begin(),ans.end(),greater<int>());
-    cout<<ans[0]+ans[1]<<"\n";
+    int r = n-1;
+    for(int i = n-1;i>=0;i--){
+        while(v[r]-v[i]>k)r--;
+        int len = r-i+1;
+        suf[i] = max((i+1<n)?suf[i+1]:0,len);
+    }
+
+    int res = 0;
+    for(int i = -1;i<n;i++){
+        int left = (i>=0)?pre[i]:0;
+        int right = (i+1<n)?suf[i+1]:0;
+        res = max(res,left+right);
+    }
+    cout<<res<<"\n";
 }
