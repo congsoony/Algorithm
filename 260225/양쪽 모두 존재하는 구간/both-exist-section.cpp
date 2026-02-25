98453ed621ee6bb425efeac3a7589e5c6cf38725
congsoony <44600941+congsoony@users.noreply.github.com>
2026-02-25 11:23:27 +0900

    Title: 양쪽 모두 존재하는 구간, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260225/양쪽 모두 존재하는 구간/both-exist-section.cpp
+++ 260225/양쪽 모두 존재하는 구간/both-exist-section.cpp
@@ -2,19 +2,30 @@
 using namespace std;
 
 int main() {
+    freopen("input.txt","r",stdin);
     int res = 1e9;
     int n,m;
     cin>>n>>m;
     vector<int>v(n);
-    for(int &i:v)cin>>i;
+    
+    unordered_map<int,int> num,outside;
+    for(int &i:v){
+        cin>>i;
+        outside[i]++;
+    }
 
-    unordered_map<int,int> num;
     int l=0;
     for(int i =0;i<n;i++){
         num[v[i]]++;
+        outside[v[i]]--;
+        if(outside[v[i]]==0){
+            outside.erase(v[i]);
+        }
         while(num.size()==m){
-            res = min(res,i-l+1);
+            if(outside.size()==m)
+                res = min(res,i-l+1);
             num[v[l]]--;
+            outside[v[l]]++;
             if(num[v[l]]==0)num.erase(v[l]);
             l++;
         }
