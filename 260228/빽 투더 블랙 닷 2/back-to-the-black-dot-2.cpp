88fe27e68e3f8fc05fc829453dd2e6b84730018d
congsoony <44600941+congsoony@users.noreply.github.com>
2026-02-28 16:41:01 +0900

    Title: 빽 투더 블랙 닷 2, Time: 70ms, Memory: 6MB, Status: Passed - Codetree

--- 260228/빽 투더 블랙 닷 2/back-to-the-black-dot-2.cpp
+++ 260228/빽 투더 블랙 닷 2/back-to-the-black-dot-2.cpp
@@ -1,22 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,m;
-int x,y;
-vector<pair<int,int>> v[100001]; 
-vector<int> dijkstra(int cur){
+#define ll long long
+ll n,m;
+ll x,y;
+vector<pair<int,ll>> v[100001]; 
+vector<ll> dijkstra(ll cur){
     priority_queue<pair<int,int>> pq;
     pq.push({0,cur});
-    vector<int> dist(n+1,1e9);
+    vector<ll> dist(n+1,1e15);
     dist[cur]=0;
     while(!pq.empty()){
-        int cost;
+        ll cost;
         tie(cost,cur)=pq.top();
         pq.pop();
         cost = -cost;
         if(dist[cur]<cost)continue;
-        for(int i =0;i<v[cur].size();i++){
-            int next = v[cur][i].first;
-            int ncost = v[cur][i].second+cost;
+        for(ll i =0;i<v[cur].size();i++){
+            ll next = v[cur][i].first;
+            ll ncost = v[cur][i].second+cost;
             if(dist[next]> ncost){
                 dist[next] = ncost;
                 pq.push({-ncost,next});
@@ -30,18 +31,18 @@ int main(){
 
     cin>>n>>m;
     cin>>x>>y;
-    int a,b,c;
-    for(int i =0;i<m;i++){
+    ll a,b,c;
+    for(ll i =0;i<m;i++){
         cin>>a>>b>>c;
         v[a].push_back({b,c});
         v[b].push_back({a,c});
     }
-    vector<int> dist1 = dijkstra(x);
-    vector<int> dist2 = dijkstra(y);
+    vector<ll> dist1 = dijkstra(x);
+    vector<ll> dist2 = dijkstra(y);
     
-    int res = 1e9;
+    ll res = 1e15;
 
-    for(int i =1;i<=n;i++){
+    for(ll i =1;i<=n;i++){
         if(i== x || i==y)continue;
         if(dist1[y] + dist2[i]== dist1[i]){
             res = min(res,2*dist1[i]);
@@ -53,5 +54,5 @@ int main(){
             res = min(res,dist1[i]*2 + dist2[i]*2);
         }
     }
-    cout<<(res==1e9?-1:res)<<"\n";
+    cout<<(res==1e15?-1:res)<<"\n";
 }
