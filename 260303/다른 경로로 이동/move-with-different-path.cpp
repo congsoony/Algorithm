f59b0d12ad2dae6320b057074bfcb5a420cbbd8e
congsoony <44600941+congsoony@users.noreply.github.com>
2026-03-03 15:37:21 +0900

    Title: 다른 경로로 이동, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260303/다른 경로로 이동/move-with-different-path.cpp
+++ 260303/다른 경로로 이동/move-with-different-path.cpp
@@ -3,10 +3,9 @@ using namespace std;
 #define ll long long
 ll n,m;
 vector<pair<ll,ll>> v[1001];
-ll parent[10001];
 set<pair<ll,ll>> check;
 vector<ll> dijkstra(ll start){
-    vector<ll> dist(n+1,1e9);
+    vector<ll> dist(n+1,1e15);
     dist[start] = 0;
     priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> pq;
     pq.push({0,start});
@@ -19,9 +18,8 @@ vector<ll> dijkstra(ll start){
             ll next = v[cur][i].first;
             ll nextCost = cost + v[cur][i].second;
             if(check.count({cur,next}))continue;
-            if(nextCost<=dist[next]){
+            if(nextCost<dist[next]){
                 dist[next] = nextCost;
-                parent[next]=cur;
                 pq.push({nextCost,next});
             }
         }
@@ -43,25 +41,24 @@ int main() {
     }
     for(int i =1;i<=n;i++)sort(v[i].begin(),v[i].end());
     
-    vector<ll> dist = dijkstra(1);
+    vector<ll> dist = dijkstra(n);
 
     ll res = 0;
-    ll cur = n;
-    vector<ll> path={n};
-    while(cur!=1){
-        for(int i=v[cur].size()-1;i>=0;i--){
-            if(dist[cur]==dist[parent[cur]]+v[cur][i].second){
-                check.insert({parent[cur],cur});
-                check.insert({cur,parent[cur]});
-                cur=parent[cur];
-                path.push_back(cur);
+    ll cur = 1;
+    while(cur!=n){
+        for(int i =0;i<v[cur].size();i++){
+            ll next = v[cur][i].first;
+            ll nextCost = v[cur][i].second;
+            if(dist[next]+nextCost==dist[cur]){
+                check.insert({cur,next});
+                check.insert({next,cur});
+                cur = next;
                 break;
             }
         }
     }
-    // reverse(path.begin(),path.end());
-    // for(ll p : path)cout<<p<<" ";
-    // cout<<"\n";
+ 
+    
     vector<ll> dist2 = dijkstra(1);
-    cout<<dist2[n]<<"\n";
+    cout<<(dist2[n]==1e15?-1:dist2[n])<<"\n";
 }
