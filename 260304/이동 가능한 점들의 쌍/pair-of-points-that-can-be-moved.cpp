148468dc4b5afb3e29bbdc24b06e1061fad767f7
congsoony <44600941+congsoony@users.noreply.github.com>
2026-03-04 15:31:38 +0900

    Title: 이동 가능한 점들의 쌍, Time: 24ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260304/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,m,p,q;
+int main() {
+    cin>>n>>m>>p>>q;
+    vector<vector<int>> w(n+1,vector<int>(n+1,1e9));
+    for(int i =0;i<m;i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        w[a][b]=c;
+    }
+    for(int i =1;i<=n;i++)w[i][i]=0;
+    for(int k=1;k<=n;k++){
+        for(int i=1;i<=n;i++){
+            for(int j =1;j<=n;j++){
+                w[i][j]= min(w[i][j],w[i][k]+w[k][j]);
+            }
+        }
+    }
+    
+    int res = 0;
+    int cnt = 0;
+
+    for(int i =0;i<q;i++){
+        int a,b;
+        cin>>a>>b;
+        int cost = 1e9;
+        for(int j =1;j<=p;j++){
+            if(w[a][j]+w[j][b]<1e9){
+                cost = min(cost,w[a][j]+w[j][b]);
+            }
+        }
+        if(cost!=1e9){
+            res +=cost;
+            cnt++;
+        }
+    }
+    cout<<cnt<<"\n";
+    cout<<res<<"\n";
+    
+
+}
