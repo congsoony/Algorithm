7dd3f435c96e2f71acb7b578bd6044450888234f
congsoony <44600941+congsoony@users.noreply.github.com>
2026-03-04 11:11:05 +0900

    Title: 같은 길, 다른 '거리', Time: 22ms, Memory: 3MB, Status: Passed - Codetree

--- 260304/같은 길, 다른 '거리'/same-road-different-distance.cpp
+++ 260304/같은 길, 다른 '거리'/same-road-different-distance.cpp
@@ -1,96 +1,83 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h> 
 using namespace std;
-
-using ll = long long;
-const ll INF = (ll)4e18;
-
-struct Edge {
-    int to;
-    int wA, wB;
-};
-
-static vector<ll> dijkstra_to_N(int N, const vector<vector<pair<int,int>>>& rev) {
-    vector<ll> dist(N + 1, INF);
-    priority_queue<pair<ll,int>, vector<pair<ll,int>>, greater<pair<ll,int>>> pq;
-
-    dist[N] = 0;
-    pq.push({0, N});
-
-    while (!pq.empty()) {
-        auto [d, x] = pq.top(); pq.pop();
-        if (d != dist[x]) continue;
-
-        for (auto [prev, w] : rev[x]) { // prev -> x with weight w
-            ll nd = d + (ll)w;
-            if (nd < dist[prev]) {
-                dist[prev] = nd;
-                pq.push({nd, prev});
+#define ll long long
+ll n,m;
+vector<ll> dijkstra(ll start,vector<pair<ll,ll>> *v){
+    vector<ll> dist(n+1,1e15);
+    dist[start] = 0;
+    priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> pq;
+    pq.push({0,start});
+    while(!pq.empty()){
+        ll cost = pq.top().first;
+        ll cur = pq.top().second;
+        pq.pop();
+        if(dist[cur]<cost) continue;
+        for(ll i =0;i<v[cur].size();i++){
+            ll next = v[cur][i].first;
+            ll nextCost = cost + v[cur][i].second;
+            if(nextCost<dist[next]){
+                dist[next] = nextCost;
+                pq.push({nextCost,next});
             }
         }
     }
     return dist;
 }
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int N, M;
-    cin >> N >> M;
-
-    vector<vector<Edge>> g(N + 1);
-    vector<vector<pair<int,int>>> revA(N + 1), revB(N + 1);
-
-    struct Raw { int u,v,a,b; };
-    vector<Raw> edges;
-    edges.reserve(M);
-
-    for (int i = 0; i < M; i++) {
-        int u, v, a, b;
-        cin >> u >> v >> a >> b;
-        g[u].push_back({v, a, b});
-        revA[v].push_back({u, a});
-        revB[v].push_back({u, b});
-        edges.push_back({u, v, a, b});
-    }
-
-    // 1) dist to N in graph A and B (run on reversed graph from N)
-    vector<ll> distA = dijkstra_to_N(N, revA);
-    vector<ll> distB = dijkstra_to_N(N, revB);
-
-    // 2) Dijkstra on warning-cost graph (edge cost = 0..2)
-    vector<int> distW(N + 1, INT_MAX);
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-    distW[1] = 0;
-    pq.push({0, 1});
-
-    while (!pq.empty()) {
-        auto [cw, u] = pq.top(); pq.pop();
-        if (cw != distW[u]) continue;
-        if (u == N) break;
-
-        for (const auto &e : g[u]) {
-            int v = e.to;
-
-            int warnA = 1;
-            if (distA[u] != INF && distA[v] != INF) {
-                if (distA[u] == distA[v] + (ll)e.wA) warnA = 0;
-            }
-
-            int warnB = 1;
-            if (distB[u] != INF && distB[v] != INF) {
-                if (distB[u] == distB[v] + (ll)e.wB) warnB = 0;
-            }
-
-            int cost = warnA + warnB; // 0..2
-            int nw = cw + cost;
-            if (nw < distW[v]) {
-                distW[v] = nw;
-                pq.push({nw, v});
+vector<ll> getPath(ll cur,ll end,vector<pair<ll,ll>> *v,vector<ll> &dist){
+    vector<ll> path={cur};
+    while(cur!=end){
+        for(int i =0;i<v[cur].size();i++){
+            ll next = v[cur][i].first;
+            ll nextCost = v[cur][i].second;
+            if(dist[next]+nextCost== dist[cur]){
+                path.push_back(next);
+                cur = next;
+                break;
             }
         }
     }
+    reverse(path.begin(),path.end());
+    return path;
+}
+int main() {
+    freopen("input.txt", "r", stdin);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n>>m;
+    vector<pair<ll,ll>> av[10001],bv[10001],rav[10001],rbv[10001];
 
-    cout << distW[N] << "\n";
-    return 0;
+    for(ll i =0;i<m;i++){
+        ll a,b,c,d;
+        cin>>a>>b>>c>>d;
+        av[a].push_back({b,c});
+        bv[a].push_back({b,d});
+        rav[b].push_back({a,c});
+        rbv[b].push_back({a,d});
+    }
+    
+    vector<ll> dist1 = dijkstra(n,rav);
+    vector<ll> dist2 = dijkstra(n,rbv);
+
+    priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> pq;
+    pq.push({0,1});
+    vector<ll>dist(n+1,1e15);
+    while(!pq.empty()){
+        ll cost = pq.top().first;
+        ll cur = pq.top().second;
+        pq.pop();
+        if(dist[cur]<cost) continue;
+        for(ll i =0;i<av[cur].size();i++){
+            ll next = av[cur][i].first;
+            ll ncost1 =av[cur][i].second;
+            ll ncost2 =bv[cur][i].second;
+            ll dif=0;
+            if(dist1[next]+ncost1 != dist1[cur]) dif++;
+            if(dist2[next]+ncost2 != dist2[cur]) dif++;
+            if(dist[next]>cost+dif){
+                dist[next] = cost+dif;
+                pq.push({dist[next],next});
+            }   
+        }
+    }
+    cout<<dist[n];
 }
