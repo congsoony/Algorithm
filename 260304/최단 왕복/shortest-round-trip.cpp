407dea7ac7989b7108c377d8cb472d6b45668c7f
congsoony <44600941+congsoony@users.noreply.github.com>
2026-03-04 12:32:52 +0900

    Title: 최단 왕복, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260304/최단 왕복/shortest-round-trip.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,m;
+int main() {
+    cin>>n>>m;
+    vector<vector<int>> w(n+1,vector<int>(n+1,1e9));
+    for(int i =0;i<m;i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        w[a][b]=c;
+    }
+    for(int k=1;k<=n;k++){
+        for(int i=1;i<=n;i++){
+            for(int j =1;j<=n;j++){
+                w[i][j]= min(w[i][j],w[i][k]+w[k][j]);
+            }
+        }
+    }
+    int res = 1e9;
+    for(int i =1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            res = min(res,w[i][j]+w[j][i]);
+        }
+    }
+    cout<<res<<"\n";
+
+    
+    
+
+}
